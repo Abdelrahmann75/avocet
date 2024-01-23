4dde7c1d0bf6dc96ae24b0dfa4f7dfdbd5160421
A El-Rahman Mohamed Mostafa <Tan7080@norpetco.com>
2024-01-23 10:46:07 +0200

    asdf

--- main_rta.py
+++ main_rta.py
@@ -117,12 +117,12 @@ ORDER By Date'''
             x = data['norm_rate']
             y = data['norm_press']
             size = data['Days']  # Add the 'Days' column for bubble size
-            date= data['Datee'].astype(str)
-            hover_data_df = pd.DataFrame({'Datee': date})
+            
+            
             
             # Plot the scatter plot
             scat = px.scatter(x=x, y=y, size=size, color_discrete_sequence=['red'], labels={'x': 'Normalized Rate', 'y': 'Normalized Pressure'},
-                              hover_data=hover_data_df)
+                              hover_data=['Days'])
             
             # Add the linear regression line to the plot
             scat.update_layout(
